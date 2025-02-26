adac1a16ae4397c83ba6054652a4c8a27d447aa6
Robertunix <managustin01@gmail.com>
2025-02-26 01:54:56 +0000

    Implemented and explored React component concepts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    /* el <> se llama fragmento y es para poder poner más de un elemento en el componente,
+    lo cual no se puede hacer*/
+    <> 
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <p>Exploring components' concepts and how they're implemented.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Text display='Hello World'/>
+      <Text display="This is the same <Text> component using a different prop."/>
     </>
   )
+}
+
+function Text({display}){
+  return (
+    <div>
+      <p>{display}</p>
+    </div>
+  );
 }
 
 export default App
